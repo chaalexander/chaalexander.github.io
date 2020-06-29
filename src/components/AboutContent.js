2d77554fb7e5cad7d5b600b5931dea09cabbbcd0
charlenne alexander <charlennep@gmail.com>
2020-06-29 17:11:23 -0500

    finish with style

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import profilePic from "../assets/images/profile.png";
 
 function AboutContent() {
-  const [showHTMLLabel, setShowHTMLLabel] = useState(false);
-  const [showJSLabel, setShowJSLabel] = useState(false);
-  const [showCSSLabel, setShowCSSLabel] = useState(false);
-  const [showReactLabel, setShowReactLabel] = useState(false);
-  const [showNPMLabel, setShowNPMLabel] = useState(false);
-  const [showNodeLabel, setShowNodeLabel] = useState(false);
-
   return (
     <>
       <div className="container" id="about">
         <div className="media w3-black">
           <img src={profilePic} className="img" alt="Profile" />
           <div className="media-body">
-            <p id="aboutme">
+            <p className="aboutmeBig">
               Brazilian full Stack Web Developer with a background in sales,
               fashion and customer service providing strong problem-solving
               skills with a keen eye for aesthetics and presentation. An
@@ -25,56 +18,11 @@ function AboutContent() {
               complex problems by contributing as a part of a fast paced,
               quality-driven team.
             </p>
-            {/* will just show on a small screen */}
-            <p className="smallScreen">
-              <i className="fab fa-html5 fa-2x"></i>{" "}
-              <i className="fab fa-js-square fa-2x"></i>{" "}
-              <i className="fab fa-css3-alt fa-2x"></i>{" "}
-              <i className="fab fa-react fa-2x"></i>{" "}
-              <i className="fab fa-npm fa-2x"></i>{" "}
-              <i className="fab fa-node fa-2x"></i>{" "}
+            <p className="aboutmeSml">
+              Brazilian full Stack Web Developer with a background in sales,
+              fashion and customer service providing strong problem-solving
+              skills with a keen eye for aesthetics and presentation.
             </p>
-            {/* will just show on a big screen */}
-            <div className="languages">
-              <ul>
-                <i
-                  className={"fab fa-html5 fa-3x lng"}
-                  onMouseEnter={() => setShowHTMLLabel(true)}
-                  onMouseLeave={() => setShowHTMLLabel(false)}
-                ></i>
-                {showHTMLLabel && <span className="display">HTML</span>}
-                <i
-                  className="fab fa-js-square fa-3x lng"
-                  onMouseEnter={() => setShowJSLabel(true)}
-                  onMouseLeave={() => setShowJSLabel(false)}
-                ></i>
-                {showJSLabel && <span className="display">JavaScript</span>}
-                <i
-                  className="fab fa-css3-alt fa-3x lng"
-                  onMouseEnter={() => setShowCSSLabel(true)}
-                  onMouseLeave={() => setShowCSSLabel(false)}
-                ></i>
-                {showCSSLabel && <span className="display">CSS</span>}
-                <i
-                  className="fab fa-react fa-3x lng"
-                  onMouseEnter={() => setShowReactLabel(true)}
-                  onMouseLeave={() => setShowReactLabel(false)}
-                ></i>
-                {showReactLabel && <span className="display">React</span>}
-                <i
-                  className="fab fa-npm fa-3x lng"
-                  onMouseEnter={() => setShowNPMLabel(true)}
-                  onMouseLeave={() => setShowNPMLabel(false)}
-                ></i>
-                {showNPMLabel && <span className="display">NPM</span>}
-                <i
-                  className="fab fa-node fa-3x lng"
-                  onMouseEnter={() => setShowNodeLabel(true)}
-                  onMouseLeave={() => setShowNodeLabel(false)}
-                ></i>
-                {showNodeLabel && <span className="display">Node</span>}
-              </ul>
-            </div>
           </div>
         </div>
       </div>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import portfolioInfo from "../../portfolio.json";
 import Cards from "../Cards";
-
+import Skills from "../Skills";
 class Portfolio extends Component {
   constructor(props) {
     super(props);
@@ -12,25 +12,30 @@ class Portfolio extends Component {
 
   render() {
     return (
-      <div className="container port" id="portfolio">
-        <div className="row">
-          {this.state.portfolioInfo.map((portfolioInfo) => (
-            <Cards
-              key={portfolioInfo.id}
-              cardImage={portfolioInfo.cardImage}
-              cardTitle={portfolioInfo.cardTitle}
-              cardText={portfolioInfo.cardText}
-              use={portfolioInfo.use}
-              use1={portfolioInfo.use1}
-              use2={portfolioInfo.use2}
-              use3={portfolioInfo.use3}
-              use4={portfolioInfo.use4}
-              liveLink={portfolioInfo.liveLink}
-              code={portfolioInfo.code}
-            />
-          ))}
+      <>
+        <div className="container port" id="portfolio">
+          <div className="row">
+            {this.state.portfolioInfo.map((portfolioInfo) => (
+              <Cards
+                key={portfolioInfo.id}
+                cardImage={portfolioInfo.cardImage}
+                cardTitle={portfolioInfo.cardTitle}
+                cardText={portfolioInfo.cardText}
+                use={portfolioInfo.use}
+                use1={portfolioInfo.use1}
+                use2={portfolioInfo.use2}
+                use3={portfolioInfo.use3}
+                use4={portfolioInfo.use4}
+                liveLink={portfolioInfo.liveLink}
+                code={portfolioInfo.code}
+              />
+            ))}
+          </div>
         </div>
-      </div>
+        <div className="skills">
+          <Skills />
+        </div>
+      </>
     );
   }
 }
