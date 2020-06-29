6296b0a3460d1e7f4b8529c88fc62eda49bb8e96
charlenne alexander <charlennep@gmail.com>
2020-06-29 17:22:13 -0500

    read me

---
+++ build/precache-manifest.69027623a609bb317ce40791b8eb880f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6595d9b595b487de68d140c7c52b85eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a376a5e2547464cc463d",
+    "url": "/static/css/main.e95d4c9e.chunk.css"
+  },
+  {
+    "revision": "48825b3d1f8ed7521434",
+    "url": "/static/js/2.76ea1c32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.76ea1c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a376a5e2547464cc463d",
+    "url": "/static/js/main.2e81ae38.chunk.js"
+  },
+  {
+    "revision": "546de4141028408d4b75",
+    "url": "/static/js/runtime-main.82ae69af.js"
+  },
+  {
+    "revision": "2dce0662fa17f45649b280023075c475",
+    "url": "/static/media/github.2dce0662.png"
+  },
+  {
+    "revision": "a7fdc20cdb7b5989ab27681d035166dc",
+    "url": "/static/media/linkedin.a7fdc20c.png"
+  },
+  {
+    "revision": "add6c89af3d52e0801047eeaa878a94e",
+    "url": "/static/media/profile.add6c89a.png"
+  },
+  {
+    "revision": "9df6136a4ecee7415eb564bc2009137c",
+    "url": "/static/media/resume.9df6136a.pdf"
+  },
+  {
+    "revision": "c6ad4a662c035ae7e389074dc487dd85",
+    "url": "/static/media/resume.c6ad4a66.png"
+  }
+]);
