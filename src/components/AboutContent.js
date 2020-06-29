688dc0e9bf6485041f92b5efc5e9eec5da83d5fb
charlenne alexander <charlennep@gmail.com>
2020-06-29 16:28:54 -0500

    added link hover for the skills

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import profilePic from "../assets/images/profile.png";
 
 function AboutContent() {
+  const [showHTMLLabel, setShowHTMLLabel] = useState(false);
+  const [showJSLabel, setShowJSLabel] = useState(false);
+  const [showCSSLabel, setShowCSSLabel] = useState(false);
+  const [showReactLabel, setShowReactLabel] = useState(false);
+  const [showNPMLabel, setShowNPMLabel] = useState(false);
+  const [showNodeLabel, setShowNodeLabel] = useState(false);
+
   return (
     <>
       <div className="container" id="about">
@@ -26,25 +33,46 @@ function AboutContent() {
               <i className="fab fa-react fa-2x"></i>{" "}
               <i className="fab fa-npm fa-2x"></i>{" "}
               <i className="fab fa-node fa-2x"></i>{" "}
-           
             </p>
             {/* will just show on a big screen */}
             <div className="languages">
-              <ul className="list-unstyled displayUnder">
-                <i className="fab fa-html5 fa-3x lng"></i>
-                <span>HTML</span>
-                <i className="fab fa-js-square fa-3x lng"></i>
-                <span className="displayUnder">JavaScript</span>{" "}
-                <i className="fab fa-css3-alt fa-3x lng"></i>{" "}
-                <span className="displayUnder">CSS</span>{" "}
-                <i className="fab fa-react fa-3x lng"></i>{" "}
-                <span className="displayUnder">React</span>{" "}
-                <i className="fab fa-npm fa-3x lng"></i>{" "}
-                <span className="displayUnder">NPM</span>{" "}
-                <i className="fab fa-node fa-3x lng"></i>{" "}
-                <span className="displayUnder">Node</span>{" "}
-                <i class="fab fa-react fa-3x lng"></i>
-                <span className="displayUnder">React</span>{" "}
+              <ul>
+                <i
+                  className={"fab fa-html5 fa-3x lng"}
+                  onMouseEnter={() => setShowHTMLLabel(true)}
+                  onMouseLeave={() => setShowHTMLLabel(false)}
+                ></i>
+                {showHTMLLabel && <span className="display">HTML</span>}
+                <i
+                  className="fab fa-js-square fa-3x lng"
+                  onMouseEnter={() => setShowJSLabel(true)}
+                  onMouseLeave={() => setShowJSLabel(false)}
+                ></i>
+                {showJSLabel && <span className="display">JavaScript</span>}
+                <i
+                  className="fab fa-css3-alt fa-3x lng"
+                  onMouseEnter={() => setShowCSSLabel(true)}
+                  onMouseLeave={() => setShowCSSLabel(false)}
+                ></i>
+                {showCSSLabel && <span className="display">CSS</span>}
+                <i
+                  className="fab fa-react fa-3x lng"
+                  onMouseEnter={() => setShowReactLabel(true)}
+                  onMouseLeave={() => setShowReactLabel(false)}
+                ></i>
+                {showReactLabel && <span className="display">React</span>}
+                <i
+                  className="fab fa-npm fa-3x lng"
+                  onMouseEnter={() => setShowNPMLabel(true)}
+                  onMouseLeave={() => setShowNPMLabel(false)}
+                ></i>
+                {showNPMLabel && <span className="display">NPM</span>}
+                <i
+                  className="fab fa-node fa-3x lng"
+                  onMouseEnter={() => setShowNodeLabel(true)}
+                  onMouseLeave={() => setShowNodeLabel(false)}
+                ></i>
+                {showNodeLabel && <span className="display">Node</span>}
               </ul>
             </div>
           </div>
