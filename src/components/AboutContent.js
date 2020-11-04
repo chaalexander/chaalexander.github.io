43b442d0189b93b556e96951bdaa451e1bccbc3e
charlenne alexander <charlennep@gmail.com>
2020-11-04 13:07:00 -0600

    added new resume and new bio

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -9,19 +9,28 @@ function AboutContent() {
           <img src={profilePic} className="img" alt="Profile" />
           <div className="media-body">
             <p className="aboutmeBig">
-              Brazilian full Stack Web Developer with a background in sales,
-              fashion and customer service providing strong problem-solving
-              skills with a keen eye for aesthetics and presentation. An
-              excellent communicator and collaborator who works well in
-              team-based projects and independently with very strong
-              interpersonal skills. Effectively leverage interest in solving
-              complex problems by contributing as a part of a fast paced,
-              quality-driven team.
+              Hi!! I’m Cha Alexander, born and raised in Brazil. I have loved
+              technology my whole life. My brother and I once took a TV apart to
+              see what was inside, and we were able to put it back together. The
+              TV turned on, there was no color or sound, but that was when I
+              decided I wanted to be a developer (Fun Fact: I was only 8). I
+              started modeling at 5 years old and began my international career
+              at the age of 18. I lived in Asia for 9 years and 1 year in
+              Europe. During my season in Holland, I visited the Philips
+              Research Center in Eindhoven and fell in love with coding. After I
+              retired from modeling I moved to the US. At the beginning of 2020
+              I signed up for the Full Stack Web Developer BootCamp at
+              Vanderbilt University, graduating with an A in the course.
+              Currently I am Interning with The Unicorn Finders company and
+              creating some websites.
             </p>
             <p className="aboutmeSml">
-              Brazilian full Stack Web Developer with a background in sales,
-              fashion and customer service providing strong problem-solving
-              skills with a keen eye for aesthetics and presentation.
+              Hi!! I’m Cha Alexander, born and raised in Brazil. Visiting the
+              Philips Research Center in Eindhoven I fell in love with coding.
+              At the beginning of 2020 I signed up for the Full Stack Web
+              Developer BootCamp at Vanderbilt University, graduating with an A
+              in the course. Currently I am Interning with The Unicorn Finders
+              company and creating some websites.
             </p>
           </div>
         </div>
